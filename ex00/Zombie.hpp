27c7c0efebf985d08816768dafc82654d4f6f68b
Salah Eddine Hali <salhali@e1r10p1.1337.ma>
2025-10-01 16:33:23 +0100

    check and push

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 18:06:35 by salhali           #+#    #+#             */
-/*   Updated: 2025/09/30 18:07:46 by salhali          ###   ########.fr       */
+/*   Updated: 2025/10/01 16:31:24 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,29 @@
 #define ZOMBIE_HPP
 
 #include <iostream>
+#include <string.h>
+
 
 class Zombie
+
 {
-    private :
-    std::string name;
-    
-    public :
-        Zombie(std::string jname) : name(jname) {};
-        void    newZombie(); // white malloc and free 
-        void    randomChump(std::string name); // whiteout malloc or free
-        void announce(void) {};
-        std::string getname() const
+    private:
+        std::string name; // donc hna 3ndi foo (dirct init); 
+    public:
+        Zombie(std::string string);
+        ~Zombie();
+        void announce( void );
+        std::string    getstring() const
         {
             return(name);
         }
+ 
 };
+
+Zombie* newZombie(std::string name ) // heap allocation
+{
+    return new Zombie(name);
+}
+void randomChump( std::string name ); // stack allocation
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,35 +6,25 @@
 /*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 16:36:11 by salhali           #+#    #+#             */
-/*   Updated: 2025/09/30 18:07:19 by salhali          ###   ########.fr       */
+/*   Updated: 2025/10/01 16:32:07 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <iostream>
-// #include <string.h>
-
-
-
-
-// // void    Zombie::announce();
-// int main()
-// {
-//     Zombie zombie("BraiiiiiiinnnzzzZ...");
-    
-//     zombie.announce();
-//     std::cout<<"string = "<< zombie.getname()<<std::endl;
-// }
 
 
 #include "Zombie.hpp"
 
-int main(){
+int main()
+{
     Zombie z1("foo");
     Zombie* z2 = newZombie("jii");
 
-    z1.announce();
-    z2->announce();
-    randomChump("maa");
-    delete z2;
+    std::cout<<"z1 = "<<z1.getstring()<<std::endl;
+    std::cout<<"z2 = "<<z2<<std::endl;
+
+    // z1.announce();
+    // z2->announce();
+    // randomChump("maa");
+    // delete z2;
     return 0;
-}⏎ 
+}
+
