3c7a71cc16b9e5a2d9817022cfe2daf00af86da7
salaheddine <salaheddineh7@gmail.com>
2025-10-09 17:42:51 +0100

    check here

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -1,14 +1,32 @@
 #include "Zombie.hpp"
 #include <iostream>
 
-Zombie::Zombie(std::string string) : name(string) {}
+// Zombie::Zombie(std::string string) : name(string) {}
+
+// Zombie::~Zombie()
+// {
+//     std::cout << name << " Destroyed." << std::endl;
+// }
+
+// void Zombie::announce()
+// {
+//     std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+// }
+
+
+// Zombie.cpp
+
+Zombie::Zombie(std::string name) : name(name)
+{
+    std::cout << "Zombie " << name << " created." << std::endl;
+}
 
 Zombie::~Zombie()
 {
-    std::cout << name << " Destroyed." << std::endl;
+    std::cout << "Zombie " << name << " destroyed." << std::endl;
 }
 
-void Zombie::announce()
-{
+void Zombie::announce() {
     std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
+
